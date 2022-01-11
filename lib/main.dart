d2355464e4e7651048b74d9e3f0c871342d36542
Bornak <bornak.paul@aimedlabs.com>
2022-01-11 21:52:41 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,89 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+	late StreamController myStreamController = StreamController();
+	late Stream myStream = myStreamController.stream.asBroadcastStream();
+
+	late StreamSubscription mySubs;
+	int data = 0;
+
+	@override
+  void initState() {
+    
+	mySubs = myStream.listen((event) { 
+		setState(() {
+		  data = event;
+		});
+	}, cancelOnError: false);
+
+	
+
+    super.initState();
+  }
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+
+        title: Text(widget.title),
+      ),
+      body: Center(
+ 
+        child: Center(
+          child: Text(
+                'The data added is $data',
+              ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+			myStreamController.sink.add(2);
+		},
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
